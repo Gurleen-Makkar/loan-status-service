f6d43b2a49e4830dfc1acee9ed713f5c980fac2a
Keerthivasan D <keerthi.vasan.d@gmail.com>
2024-06-03 21:57:08 +0530

    Initialise springboot project

---
+++ src/main/java/com/copilot/loan_status_service/LoanStatusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.copilot.loan_status_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanStatusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanStatusServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/copilot/loan_status_service/LoanStatusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copilot.loan_status_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanStatusServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
